5fba01c650332005ea7b4f48085fce01ad83c3b3
Evhen Gruzinov <evhen@gruzinov.com>
2023-09-10 12:34:30 +0200

    Initial Commit

---
+++ Spilka/SpilkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpilkaApp.swift
+//  Spilka
+//
+//  Created by Evhen Gruzinov on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpilkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
